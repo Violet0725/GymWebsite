a096e7340349d9552a1a8df9aeba5fbdec318f58
Eric Zhou <107652265+Violet0725@users.noreply.github.com>
2024-08-03 01:27:51 -0400

    Add files via upload

---
+++ src/utils/fetchData.js
@@ -0,0 +1,27 @@
+export const exerciseOptions = {
+  method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises',
+  params: {
+    limit: '10',
+    offset: '0'
+  },
+  headers: {
+    'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+    'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
